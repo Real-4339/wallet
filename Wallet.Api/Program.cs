57b73a08a0083d5feb572c9d6223c189b318b93b
Real-4339 <qvadym@stuba.sk>
2023-05-16 17:27:09 +0200

    added service collections

--- Wallet.Api/Program.cs
+++ Wallet.Api/Program.cs
@@ -1,16 +1,19 @@
+using Wallet.DI;
+
 var builder = WebApplication.CreateBuilder(args);
-{
+{   
     // Add services to the container.
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure(builder.Configuration);
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
 }
 
 var app = builder.Build();
 {
-
     app.UseHttpsRedirection();
     app.MapControllers();
 
     app.Run();
-
 }
