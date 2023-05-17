7f918f14e03c7e1abc5287caae0409acfabad29a
Real-4339 <qvadym@stuba.sk>
2023-05-18 01:37:46 +0200

    reg validator

---
+++ src/Application/Auth/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,30 @@
+using FluentValidation;
+
+namespace Application.Auth.Commands.Register;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            .MaximumLength(50).WithMessage("First name must not exceed 50 characters");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required")
+            .MaximumLength(50).WithMessage("Last name must not exceed 50 characters");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Email is invalid");
+
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage("Username is required")
+            .MaximumLength(50).WithMessage("Username must not exceed 50 characters");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters")
+            .MaximumLength(50).WithMessage("Password must not exceed 50 characters");
+    }
+}
