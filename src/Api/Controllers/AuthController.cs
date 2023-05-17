2ecd2902ef3d7fc6aced7e98f44be23c13bc37e5
Real-4339 <qvadym@stuba.sk>
2023-05-17 22:33:49 +0200

    used mapper ( mapster )

--- src/Api/Controllers/AuthController.cs
+++ src/Api/Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using Application.Auth.Commands.Register;
 using Application.Auth.Queries.Login;
 using Microsoft.AspNetCore.Mvc;
 using Dtos.Authentication;
+using MapsterMapper;
 using MediatR;
 
 namespace Api.Controllers;
@@ -10,45 +11,30 @@ namespace Api.Controllers;
 public class AuthController : ApiController
 {
     private readonly ISender _mediator;
+    private readonly IMapper _mapper;
 
     public AuthController(
-        ISender mediator)
+        ISender mediator,
+        IMapper mapper)
     {
         _mediator = mediator;
+        _mapper = mapper;
     }
 
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterRequest request)
     {   
-        var command = new RegisterCommand(
-            request.firstName, 
-            request.lastName, 
-            request.Email,
-            request.Username, 
-            request.Password
-        );
+
+        var command = _mapper.Map<RegisterCommand>(request);
         var authRegResult = await _mediator.Send(command);
 
-        var response = new RegisterResponse(
-            authRegResult.User.Id,
-            authRegResult.User.Username, 
-            authRegResult.Token
-        );
-
-        return Ok(response);
+        return Ok(_mapper.Map<RegisterResponse>(authRegResult));
     }
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginRequest request)
     {
-        var authLogResult = await _mediator.Send(new LoginQuery(
-            request.Username, 
-            request.Password
-        ));
+        var authLogResult = await _mediator.Send(_mapper.Map<LoginQuery>(request));
 
-        var response = new LoginResponse(
-            authLogResult.Status
-        );
-
-        return Ok(response);
+        return Ok(_mapper.Map<LoginResponse>(authLogResult));
     }
 }
