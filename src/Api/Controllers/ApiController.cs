5889851d0869ba48b02d6c6674a37626ad74c299
Real-4339 <qvadym@stuba.sk>
2023-05-18 20:11:17 +0200

    added Authorize decorator

--- src/Api/Controllers/ApiController.cs
+++ src/Api/Controllers/ApiController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
 [ApiController]
+[Authorize]
 public abstract class ApiController : ControllerBase
 {
     protected ApiController()
