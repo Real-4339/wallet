0fcc3fe45eea45a73d4760f07fcfd383b1c419d1
Real-4339 <qvadym@stuba.sk>
2023-05-18 20:12:35 +0200

    added authorization

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,10 +1,14 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Application.Common.Interfaces.Persistence;
 using Microsoft.Extensions.DependencyInjection;
 using Application.Common.Interfaces.Services;
 using Application.Common.Interfaces.Auth;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Options;
 using Infrastucture.Persistence;
 using Infrastructure.Auth;
+using System.Text;
 
 namespace Infrastructure;
 
@@ -13,13 +17,41 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services, 
         ConfigurationManager configuration)
-    {   
-        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
-        
-        services.AddScoped<IJwtTokenGenerator, JwtTokenGen>();
-        services.AddScoped<IDateTimeProvider, DateTimeProvider>();
-        
+    {  
+        services.AddAuth(configuration);
+        services.AddScoped<IDateTimeProvider, DateTimeProvider>(); 
         services.AddSingleton<IUserRepo, UserRepo>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddAuth(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SectionName, jwtSettings);
+        
+        services.AddSingleton(Options.Create(jwtSettings));
+        services.AddScoped<IJwtTokenGenerator, JwtTokenGen>();
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings.Secret)),
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
 
         return services;
     }
