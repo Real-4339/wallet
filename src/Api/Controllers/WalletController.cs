17468c21e6fa099649ae6715208a7de6f4203ce5
Real-4339 <qvadym@stuba.sk>
2023-05-19 13:18:24 +0200

    register wallet

--- src/Api/Controllers/WalletController.cs
+++ src/Api/Controllers/WalletController.cs
@@ -1,13 +1,34 @@
+using Application.Users.Commands.Wallet;
 using Microsoft.AspNetCore.Mvc;
+using Dtos.User.Wallet;
+using MapsterMapper;
+using MediatR;
+using Dtos.User;
 
 namespace Api.Controllers;
 
-[Route("[controller]")]
+[Route("users/{userId}/wallet")]
 public class WalletController : ApiController
-{
-    [HttpGet]
-    public async Task<IActionResult> Get()
+{   
+    private readonly ISender _mediator;
+    private readonly IMapper _mapper;
+
+    public WalletController(
+        ISender mediator,
+        IMapper mapper)
     {
-        return Ok(Array.Empty<string>());
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        RegisterWalletRequest request,
+        string userId)
+    {   
+        var command = new RegisterWalletCommand(userId);
+        var walletResult = await _mediator.Send(command);
+
+        return Ok(_mapper.Map<RegisterWalletResponse>(walletResult));
     }
 }
