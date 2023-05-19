c2002d60ba6802fd73ff1a98edb00e74b43e1b10
Real-4339 <qvadym@stuba.sk>
2023-05-19 14:02:27 +0200

    now user is an aggroot

--- src/Domain/UserAggregate/User.cs
+++ src/Domain/UserAggregate/User.cs
@@ -1,14 +1,53 @@
-using Domain.Common.Primitives;
 using Domain.UserAggregate.ValueObjects;
+using Domain.UserAggregate.Entities;
+using Domain.Common.Primitives;
 
 namespace Domain.UserAggregate;
 
-public class User // : AggregateRoot<UserId>
+public sealed class User : AggregateRoot<UserId>
 {
-    public Guid Id { get; set; }
-    public string firstName { get; set; } = null!;
-    public string lastName { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Username { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public string firstName { get; private set; }
+    public string lastName { get; private set; }
+    public string Email { get; private set; }
+    public string Username { get; private set; }
+    public string Password { get; private set; }
+    public UserWallet Wallet { get; private set; } = default!;
+
+    private User(
+        UserId userId,
+        string firstname,
+        string lastname,
+        string email,
+        string username,
+        string password)
+        : base(userId)
+    {
+        firstName = firstname;
+        lastName = lastname;
+        Email = email;
+        Username = username;
+        Password = password;
+    }
+
+    public static User Create(
+        string firstname,
+        string lastname,
+        string email,
+        string username,
+        string password) =>
+        new(
+            UserId.New(),
+            firstname,
+            lastname,
+            email,
+            username,
+            password);
+        
+    public void RegisterWallet(decimal balance){
+        if (Wallet != null){
+            throw new Exception("User already has a wallet");
+        }
+        Wallet = UserWallet.Create(balance);
+    }
+
 }
