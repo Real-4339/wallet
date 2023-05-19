775a65e894731e8a951f5ceeec3f47ee334c3f07
Real-4339 <qvadym@stuba.sk>
2023-05-19 18:50:42 +0200

    get balance endpoint

--- src/Api/Controllers/WalletController.cs
+++ src/Api/Controllers/WalletController.cs
@@ -1,5 +1,7 @@
 using Application.Users.Commands.Wallet;
+using Application.Users.Queries.Balance;
 using Microsoft.AspNetCore.Mvc;
+using Dtos.User.Balance;
 using Dtos.User.Wallet;
 using MapsterMapper;
 using MediatR;
@@ -30,5 +32,17 @@ public class WalletController : ApiController
         var walletResult = await _mediator.Send(command);
 
         return Ok(_mapper.Map<RegisterWalletResponse>(walletResult));
+    }
+
+    [HttpGet]
+    [Route("balance")]
+    public async Task<IActionResult> Get(
+        GetBalanceRequest request,
+        Guid userId)
+    {   
+        var query = new GetBalanceQuery(userId);
+        var walletResult = await _mediator.Send(query);
+
+        return Ok(_mapper.Map<GetBalanceResponse>(walletResult));
     }
 }
