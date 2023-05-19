c64307677a6597d15013b673f3f2614247acad6f
Real-4339 <qvadym@stuba.sk>
2023-05-19 11:25:09 +0200

    ValueObject

---
+++ src/Domain/User/ValueObjects/UserWalletId.cs
@@ -0,0 +1,20 @@
+using Domain.Common.Primitives;
+
+namespace Domain.User.ValueObjects;
+
+public sealed class UserWalletId : ValueObject
+{
+    private UserWalletId(Guid value)
+    {
+        Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public static UserWalletId New() => new(Guid.NewGuid());
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+} 
