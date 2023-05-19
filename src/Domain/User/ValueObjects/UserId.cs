a0c7cbc265e3577b4f642a3c23f52199c7b805bd
Real-4339 <qvadym@stuba.sk>
2023-05-19 11:25:03 +0200

    ValueObject

--- src/Domain/User/ValueObjects/UserId.cs
+++ src/Domain/User/ValueObjects/UserId.cs
@@ -3,13 +3,13 @@ using Domain.Common.Primitives;
 namespace Domain.User.ValueObjects;
 
 public sealed class UserId : ValueObject
-{
+{   
+    public Guid Value { get; }
+
     private UserId(Guid value)
     {
         Value = value;
     }
-
-    public Guid Value { get; }
 
     public static UserId New() => new(Guid.NewGuid());
 
