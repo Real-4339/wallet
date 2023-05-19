701f58a9a1166e0fbd7aeb3138ec37789717ff5f
Real-4339 <qvadym@stuba.sk>
2023-05-19 11:24:36 +0200

    added Tx AggRoot

---
+++ src/Domain/Transactions/Tx.cs
@@ -0,0 +1,31 @@
+using Domain.Transactions.ValueObjects;
+using Domain.User.ValueObjects;
+using Domain.Common.Primitives;
+
+namespace Domain.Transactions;
+
+public sealed class Tx : AggregateRoot<TxId>
+{
+    public UserId UserId { get; }
+    public string Type { get; }
+    public decimal Amount { get; }
+    
+    private Tx(
+        TxId id, 
+        UserId userId, 
+        string type, 
+        decimal amount)
+        : base(id)
+    {
+        UserId = userId;
+        Type = type;
+        Amount = amount;
+    }
+
+    public static Tx Create(
+        UserId userId, 
+        string type, 
+        decimal amount) => 
+        new(TxId.New(), userId, type, amount);
+}
+    
