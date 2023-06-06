4a4bb2af4a147ba307099116d20af7eb44c8931a
Real-4339 <qvadym@stuba.sk>
2023-06-06 13:56:55 +0200

    added handle for upper and lower cases

--- src/Application/Users/Commands/Transactions/CreditTxValidator.cs
+++ src/Application/Users/Commands/Transactions/CreditTxValidator.cs
@@ -3,13 +3,13 @@ using FluentValidation;
 namespace Application.Users.Commands.Transactions;
 
 public class CreditTxValidator : AbstractValidator<CreditTxCommand>
-{
+{   
     public CreditTxValidator()
     {   
         // Only takes win, stake, credit.
         RuleFor(x => x.Type)
             .NotEmpty().WithMessage("Transaction is required")
-            .Must(x => x == "win" || x == "stake" || x == "deposit")
+            .Must(x => x.ToLower() == "win" || x.ToLower() == "stake" || x.ToLower() == "deposit")
                     .WithMessage("Transaction must be win, stake, or deposit");
     }
 }
