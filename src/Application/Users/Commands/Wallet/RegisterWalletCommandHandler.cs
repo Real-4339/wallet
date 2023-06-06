03e16d85654a6b116d736de47e74748ffddfd5f7
Real-4339 <qvadym@stuba.sk>
2023-06-06 13:48:22 +0200

    added walletSemaphore

--- src/Application/Users/Commands/Wallet/RegisterWalletCommandHandler.cs
+++ src/Application/Users/Commands/Wallet/RegisterWalletCommandHandler.cs
@@ -8,26 +8,29 @@ namespace Application.Users.Commands.Wallet;
 public class RegisterWalletCommandHandler : IRequestHandler<RegisterWalletCommand, StatusResult>
 {   
     private readonly IUserRepo _authRepository;
+    private readonly ISemaphoreRepo _semaphore;
 
     public RegisterWalletCommandHandler(
-        IUserRepo authRepository
-        )
+        IUserRepo authRepository,
+        ISemaphoreRepo semaphore)
     {   
         _authRepository = authRepository;
+        _semaphore = semaphore;
     }
 
     public async Task<StatusResult> Handle(RegisterWalletCommand command, CancellationToken cancellationToken)
     {   
-        await Task.CompletedTask;
-
         // Find user
-        if (_authRepository.GetUserById(command.UserId) is not User user)
+        if (await _authRepository.GetUserByIdAsync(command.UserId) is not User user)
         {   
             throw new Exception("Internal Server Error");
         }
-        // Register wallet
-        user.RegisterWallet(0);
 
+        // Register wallet
+        await _semaphore.WalletWaitAsync();
+        user.RegisterWallet(0);
+        _semaphore.WalletSemaphore.Release();
+        
         // Return status result
         return new StatusResult("success");
     }
