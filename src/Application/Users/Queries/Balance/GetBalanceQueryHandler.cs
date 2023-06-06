62956b7e7bc89e951d3a26348d8bcd7a0c676aaf
Real-4339 <qvadym@stuba.sk>
2023-06-06 13:52:04 +0200

    added wallet sema

--- src/Application/Users/Queries/Balance/GetBalanceQueryHandler.cs
+++ src/Application/Users/Queries/Balance/GetBalanceQueryHandler.cs
@@ -7,25 +7,29 @@ namespace Application.Users.Queries.Balance;
 public class BalanceQueryHandler : IRequestHandler<GetBalanceQuery, GetBalanceResult>
 {
     private readonly IUserRepo _authRepository;
+    private readonly ISemaphoreRepo _semaphore;
 
     public BalanceQueryHandler(
-        IUserRepo authRepository
-        )
+        IUserRepo authRepository,
+        ISemaphoreRepo semaphore)
     {   
         _authRepository = authRepository;
+        _semaphore = semaphore;
     }
 
     public async Task<GetBalanceResult> Handle(GetBalanceQuery query, CancellationToken cancellationToken)
     {   
-        await Task.CompletedTask;
-
         // Find user
-        if (_authRepository.GetUserById(query.UserId) is not User user)
-        {   
+        if (await _authRepository.GetUserByIdAsync(query.UserId) is not User user)
+        {
             throw new Exception("Internal Server Error");
         }
 
+        await _semaphore.WalletWaitAsync();
+        var balance = user.GetBalance();
+        _semaphore.WalletSemaphore.Release();
+
         // Return balance result
-        return new GetBalanceResult(user.Id.Value, user.GetBalance());
+        return new GetBalanceResult(user.Id.Value, balance);
     }
 }
